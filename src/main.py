911b74df2ce9affaac11c3685775747196d312fb
WeslyG <weslyg22@gmail.com>
2020-05-25 16:31:45 +0500

    add style

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@
 # coding=utf-8
 
 import click
+import os
 
 @click.group()
 def cli():
@@ -13,8 +14,13 @@ def install(name):
     '''
     install dependency from meta/main.yml in local folder
     '''
-    print('hey you')
-
+    if os.system("ansible-galaxy --version > /dev/null ") != 1:
+        click.echo(click.style(
+          'Ansible-galaxy not found!\
+          You can manualy set path to ansible-galaxy bin,\
+          use GALAXY_PATH env, or galaxy_path config options',
+          fg='red'),
+        err=True)
 
 if __name__ == '__main__':
     cli()
